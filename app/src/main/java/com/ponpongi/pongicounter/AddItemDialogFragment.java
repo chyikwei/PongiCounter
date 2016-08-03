fd13fa4e0c4277a8984e4793ee7f8bcca4f99b21
chyikwei <chyikwei.yau@gmail.com>
2016-08-02 22:38:26 -0400

    add "add" button and function in action bar

---
+++ app/src/main/java/com/ponpongi/pongicounter/AddItemDialogFragment.java
@@ -0,0 +1,57 @@
+package com.ponpongi.pongicounter;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+/**
+ * Created by chyikwei on 8/2/2016.
+ */
+public class AddItemDialogFragment extends DialogFragment implements OnEditorActionListener {
+
+    public interface EditNewItemDialogListener {
+        void onFinishEditDialog(String inputText);
+    }
+
+    private EditText mEditText;
+
+    public AddItemDialogFragment() {
+        //empty
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_item, container);
+        mEditText = (EditText) view.findViewById(R.id.fragment_title_name);
+        getDialog().setTitle("New Item");
+
+        // Show soft keyboard automatically
+        mEditText.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        mEditText.setOnEditorActionListener(this);
+
+        return view;
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (EditorInfo.IME_ACTION_DONE == actionId) {
+            // Return input text to activity
+            EditNewItemDialogListener activity = (EditNewItemDialogListener) getActivity();
+            activity.onFinishEditDialog(mEditText.getText().toString());
+            this.dismiss();
+            return true;
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/ponpongi/pongicounter/MainActivity.java
+++ app/src/main/java/com/ponpongi/pongicounter/MainActivity.java
@@ -1,19 +1,26 @@
 package com.ponpongi.pongicounter;
 
 import android.content.Context;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.ponpongi.pongicounter.AddItemDialogFragment.EditNewItemDialogListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EditNewItemDialogListener {
 
     private StaggeredGridLayoutManager sg_manager;
+    private ItemAdapter adapter;
+    List<CounterItem> data_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         final Context context = getApplicationContext();
 
         // references to our images
-        List<CounterItem> data_list = new ArrayList<CounterItem>();
-        for (int i=1; i <= 20; i++) {
+        data_list = new ArrayList<CounterItem>();
+        //TODO: load real data
+        for (int i=1; i <= 2; i++) {
             data_list.add(new CounterItem("Item " + i));
         }
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.card_grid);
@@ -31,27 +39,42 @@ public class MainActivity extends AppCompatActivity {
         sg_manager = new StaggeredGridLayoutManager(2, 1);
         recyclerView.setLayoutManager(sg_manager);
 
-        ItemAdapter adapter = new ItemAdapter(data_list);
+        adapter = new ItemAdapter(data_list);
         recyclerView.setAdapter(adapter);
-        /*
-        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
-                CounterItem item = items[pos];
-                item.increment();
-                TextView it = (TextView) view.findViewById(R.id.item_title);
-                TextView ic = (TextView) view.findViewById(R.id.item_count);
-                it.setText(item.getName());
-                ic.setText((Integer.toString(item.getCount())));
-                Toast.makeText(context, item.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-        */
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                showAddDialog();
+                return true;
+            case R.id.list_item:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showAddDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        AddItemDialogFragment dialogFragment = new AddItemDialogFragment();
+        dialogFragment.setStyle(DialogFragment.STYLE_NORMAL, R.style.CustomDialog);
+        dialogFragment.show(fm, "fragment_add_item");
+    }
+
+    @Override
+    public void onFinishEditDialog(String inputText) {
+        data_list.add(new CounterItem(inputText));
+        adapter.notifyItemInserted(data_list.size() - 1);
+
+        Toast.makeText(this, "Add item " + inputText, Toast.LENGTH_SHORT).show();
     }
 }
